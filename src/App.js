f6df86ee30d1009b120511ab2873a237c3b27879
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-03-08 01:20:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+
+
+function App() {
+  return (
+    <>
+      <div>
+        Hello world!
+      </div>
+      <Stack spacing={2} direction="row">
+        Hello world
+      </Stack>
+    </>
+  );
+}
+
+export default App;
