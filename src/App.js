fa06608a8a7f72522b5c60194ac32283f31e5f1a
ANANYA TRIPATHI <ananyatrip0802@gmail.com>
2023-04-07 00:02:38 +0530

    Define Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import Stack from '@mui/material/Stack';
-
+import React from "react";
+import Stack from "@mui/material/Stack";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
   return (
     <>
-      <div>
-        Hello world!
-      </div>
-      <Stack spacing={2} direction="row">
-        Hello world
-      </Stack>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route
+            path={"/clubs"}
+            element={<p>clubs I'm in component folder</p>}
+          />
+          <Route
+            path={"/clubs"}
+            element={<p>clubs I'm in component folder</p>}
+          />
+        </Route>
+      </Routes>
     </>
   );
 }
