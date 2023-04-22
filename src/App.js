22f0dee8982eaee1b60a116d244195428877a902
VanshikaRana1432 <vanshikarana1432@gmail.com>
2023-04-22 11:58:12 +0530

    cubPages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
-
+import {Sportsclub} from './club/Sportsclub'
 
 function App() {
   return (
     <>
       <div>
-        Hello world!
+        <Sportsclub/>
       </div>
       <Stack spacing={2} direction="row">
         Hello world
