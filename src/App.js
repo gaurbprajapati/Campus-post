034a9b1b0f456fa9b27bc3e3086b0676fc07250b
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-04-22 22:14:18 +0530

    cleaning app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Stack from '@mui/material/Stack';
+
 
 
 function App() {
   return (
     <>
-      <div>
-        Hello world!
-      </div>
-      <Stack spacing={2} direction="row">
-        Hello world
-      </Stack>
+
     </>
   );
 }
