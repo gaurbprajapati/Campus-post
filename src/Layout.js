279f38f4bf42f6df0c58c56a1eca2aed99810f6f
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-04-26 00:52:39 +0530

    loyout improve

--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from './Pages/Home/NavBar'
+import Footer from './Components/Footer/Footer'
 
 const Layout = () => {
-  return (
-    <>
-      <p>"header" I'm present in Layout.js </p>
-      <Outlet />
-    </>
-  );
-};
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
 
 export default Layout;
+
