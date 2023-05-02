3885c7186e673f774cbc28db5a5f1eeceb94b12c
ANANYA TRIPATHI <ananyatrip0802@gmail.com>
2023-05-02 12:06:23 +0530

    fix scrolll issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import IndexPage from "./Pages/Home/IndexPage.jsx";
 import NavBar from "./Pages/Home/NavBar.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Sportclub from "./club/Sportsclub.jsx";
 import AboutUs from "./Pages/Home/AboutUs.jsx";
+// import Footer from "./Components/Footer/Footer.js";
+import Resource from "./Components/resources/Resource.jsx";
+// function ScrollToTop() {
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const scrollHandler = () => window.scrollTo(0, 0);
+//     navigate(scrollHandler);
+//   }, [navigate]);
+
+//   return null;
+// }
 
 function App() {
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const scrollHandler = () => window.scrollTo(0, 0);
+  //   console.log("chutiya");
+  //   navigate(scrollHandler);
+  // }, [navigate]);
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/sportsclub" element={<Sportclub />} />
-          <Route path="/Home" element={<IndexPage />} />
-          <Route path="/About" element={<AboutUs />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/sportsclub" element={<Sportclub />} />
+        <Route path="/Home" element={<IndexPage />} />
+        <Route path="/About" element={<AboutUs />} />
+        <Route path="/Resources" element={<Resource />} />
+        {/* <Route path="/*" element={<ScrollToTop />} /> */}
+      </Routes>
     </>
   );
 }
