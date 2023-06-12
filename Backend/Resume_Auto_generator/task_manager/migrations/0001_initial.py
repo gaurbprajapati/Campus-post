270c27732b880b5b373f6fd58d16cd9b5edbaf8a
Restyled.io <commits@restyled.io>
2023-06-12 18:59:30 +0000

    Restyled by autopep8

--- Backend/Resume_Auto_generator/task_manager/migrations/0001_initial.py
+++ Backend/Resume_Auto_generator/task_manager/migrations/0001_initial.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
                 ('description', models.TextField()),
             ],
--- Backend/Resume_Auto_generator/taskmanager/urls.py
+++ Backend/Resume_Auto_generator/taskmanager/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,include
-# from task_manager.url 
+from django.urls import path, include
+# from task_manager.url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('task_manager.urls'))
-    
+    path('', include('task_manager.urls'))
+
 ]
