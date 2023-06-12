964789880a17b7a09361b5be10ff5e84715401e9
Restyled.io <commits@restyled.io>
2023-06-12 18:59:33 +0000

    Restyled by black

--- Backend/Resume_Auto_generator/task_manager/migrations/0001_initial.py
+++ Backend/Resume_Auto_generator/task_manager/migrations/0001_initial.py
@@ -4,20 +4,25 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Task',
+            name="Task",
             fields=[
-                ('id', models.BigAutoField(auto_created=True,
-                 primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('description', models.TextField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("description", models.TextField()),
             ],
         ),
     ]
--- Backend/Resume_Auto_generator/taskmanager/urls.py
+++ Backend/Resume_Auto_generator/taskmanager/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 # from task_manager.url
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('task_manager.urls'))
-
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("task_manager.urls"))]
