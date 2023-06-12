b30d8b48d61fdd3b52dda7120dd05a7600d1fa87
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-06-13 01:39:47 +0530

    cookie setup

--- Backend/utils/features.js
+++ Backend/utils/features.js
@@ -2,11 +2,12 @@ import jwt from "jsonwebtoken";
 
 export const sendCookie = (user, res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  const duration = 10 * 60e3;
   res
     .status(statusCode)
     .cookie("token", token, {
       httpOnly: true,
-      maxAge: 15 * 60 * 1000,
+      maxAge: duration,
     })
     .json({
       success: true,
